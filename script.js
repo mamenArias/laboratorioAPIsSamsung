3af1de90ea4687f3bf7f4074f0e36dc378d9de96
Mª Carmen Arias <mcariasdeharo@gmail.com>
2023-06-05 19:58:03 +0200

    Ejercicio finalizado

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
 if (window.File && window.FileReader && window.FileList) {
-    const fileVideo = document.getElementById('fileVideo');
+    const fileInput = document.getElementById('fileInput');
+    const video = document.getElementById('video');
+    const videoContainer = document.getElementById('videoContainer');
+    const loading = document.getElementById('loading');
+    const buttons = document.getElementById('buttons');
+    const play = document.getElementById('play');
+    const pause = document.getElementById('pause');
+    const volumeUp = document.getElementById('volumeUp');
+    const volumeDown = document.getElementById('volumeDown');
 
-    fileVideo.addEventListener ("change", () => {
-        const file = fileVideo.files[0];
-        const ext = /(.mp4)$/i;
-        if (!ext.exec(file)){
-            console.log("Es correcto");
+    fileInput.addEventListener ('change', function(e) {
+        const file = fileInput.files[0];
+
+        if (file && file.type.startsWith('video/')){
+            
+            loading.style.display = 'block';
+
+            const fileURL = URL.createObjectURL(file);
+            video.src = fileURL;
+
+            setTimeout(function(){
+                loading.style.display = 'none';
+                videoContainer.style.display = 'block';
+                buttons.style.display = 'block';
+            }, 2000);
+
+            play.addEventListener('click', function(){
+                video.play();
+            });
+
+            pause.addEventListener('click', function(){
+                video.pause();
+            });
+
+            volumeUp.addEventListener('click', function(){
+                video.volume += 0.1;
+            });
+
+            volumeDown.addEventListener('click', function(){
+                video.volume -= 0.1;
+            });
+
         } else {
-            alert("Por favor, seleccione un tipo de archivo válido.");
-            file.value='';
-            return false;
+            alert('Por favor, seleccione un tipo de archivo válido.');
         }
     } )
 } else {
